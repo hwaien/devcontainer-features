c64897c0660ebb4c445ac948efbb504772975ed0
hwaien <1613381+hwaien@users.noreply.github.com>
2023-03-02 16:47:44 -0800

    Test Automation (#2)

---
+++ test/match-host-time-zone/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This test file will be executed against an auto-generated devcontainer.json that
+# includes the 'hello' Feature with no options.
+#
+# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
+#
+# The value of all options will fall back to the default value in 
+# the Feature's 'devcontainer-feature.json'.
+#
+# These scripts are run as 'root' by default. Although that can be changed
+# with the '--remote-user' flag.
+
+set -e
+
+# Import test library bundled with the devcontainer CLI
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "time-zone-not-utc" date
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
