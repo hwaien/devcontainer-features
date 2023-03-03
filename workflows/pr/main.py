5b28ea92e23492068defa3f095eab4ed14b55974
Hwai-En Ho <dev@hwaien.com>
2023-03-03 23:58:21 +0000

    recurse

--- workflows/pr/main.py
+++ workflows/pr/main.py
@@ -1,12 +1,19 @@
 import os
-from github import Github
+from github import Github, Repository
+
+def get_contents(repo: Repository, path: str, ref: str):
+    for child in repo.get_contents(path, ref):
+        if (child.type == 'dir'):
+            for descendant in get_contents(repo, child.path, ref):
+                yield descendant
+        else:
+            yield child
 
 def main():
     gh = Github(os.environ['GITHUB_TOKEN'])
     repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
-    branch = os.environ['GITHUB_REF_NAME']
-    files = repo.get_contents('', branch)
-    for f in files:
+    ref = os.environ['GITHUB_REF_NAME']
+    for f in get_contents(repo, '', ref):
         print(f.path)
         print(f.type)
 
