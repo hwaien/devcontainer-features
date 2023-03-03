74f4d135dc04105610d72ada3781f0e3f53b5e14
Hwai-En Ho <dev@hwaien.com>
2023-03-03 23:41:57 +0000

    adding python script

---
+++ workflows/pr/main.py
@@ -0,0 +1,13 @@
+from github import Github
+
+
+def main():
+    gh = Github(os.environ['GITHUB_TOKEN'])
+    repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
+    branch = os.environ['GITHUB_REF_NAME']
+    files = repo.get_contents('', branch)
+    for f in files:
+        print(f.path)
+
+if __name__ == "__main__":
+    main()
