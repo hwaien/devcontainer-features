200ed172b1b52cc72a78827e7113cbc297c2e4ec
Hwai-En Ho <dev@hwaien.com>
2023-03-03 01:01:57 +0000

    clarification

--- test/match-host-time-zone/test.sh
+++ test/match-host-time-zone/test.sh
@@ -19,7 +19,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "time-zone-not-utc" date
+check "date-available" date
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
