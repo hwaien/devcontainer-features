d7af126e740d7cd6dc11d33ab8cb14c1f4e4485f
Hwai-En Ho <dev@hwaien.com>
2023-03-04 23:33:13 +0000

    fixing error

--- workflows/bump/main.py
+++ workflows/bump/main.py
@@ -1,7 +1,7 @@
 from glob import glob
 
 def main():
-    for item in glob('src/*/devcontainer-feature.json')
+    for item in glob('src/*/devcontainer-feature.json'):
         print(item)
 
 if __name__ == "__main__":
