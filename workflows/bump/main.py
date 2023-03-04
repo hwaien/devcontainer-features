d96828ea5628da5a6bbe2728bb14d616a69edc83
hwaien <1613381+hwaien@users.noreply.github.com>
2023-03-04 15:52:37 -0800

    Automatic Version Bump (#8)
    Automatically updating versions of features, to prevent manual version updates being forgotten.

---
+++ workflows/bump/main.py
@@ -0,0 +1,23 @@
+from glob import glob
+import json
+from semver import VersionInfo
+
+def bump(path: str):
+    with open(path) as inFile:
+        fileContent = json.load(inFile)
+    originalVersionString = fileContent['version']
+    originalVersion = VersionInfo.parse(originalVersionString)
+    bumpedVersion = originalVersion.bump_patch()
+    bumpedVersionString = str(bumpedVersion)
+    fileContent['version'] = bumpedVersionString
+    print(path)
+    print(fileContent)
+    with open(path, 'w') as outFile:
+        json.dump(fileContent, outFile, indent=4)
+
+def main():
+    for metadata in glob('src/*/devcontainer-feature.json'):
+        bump(metadata)
+
+if __name__ == "__main__":
+    main()
