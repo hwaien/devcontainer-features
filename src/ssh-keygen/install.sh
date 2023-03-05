dfb95be8d39d40aa193d6c798a49dfeab7bd9745
Hwai-En Ho <dev@hwaien.com>
2023-03-05 04:15:15 +0000

    debugging

--- src/ssh-keygen/install.sh
+++ src/ssh-keygen/install.sh
@@ -2,8 +2,25 @@
 
 echo "Generating SSH key"
 
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
 echo $_REMOTE_USER_HOME
 
 mkdir -p $_REMOTE_USER_HOME/.ssh/
+
+check_packages openssh-clients
 
 ssh-keygen -t rsa -N '' -f $_REMOTE_USER_HOME/.ssh/id_rsa
