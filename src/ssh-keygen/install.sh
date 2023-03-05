022ef5993f92df7d81fd5f89ecf822a99b36348f
Hwai-En Ho <dev@hwaien.com>
2023-03-05 04:25:17 +0000

    debugging

--- src/ssh-keygen/install.sh
+++ src/ssh-keygen/install.sh
@@ -7,7 +7,10 @@ apt_get_update()
     if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
         apt-get update -y
+        echo "/etc/apt/sources.list"
         cat /etc/apt/sources.list
+        echo "ls /etc/apt/sources.list.d"
+        ls /etc/apt/sources.list.d
     fi
 }
 
