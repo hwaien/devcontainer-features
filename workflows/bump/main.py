e9c201c672271eefee0a5e9b30383af4c2ded818
Hwai-En Ho <dev@hwaien.com>
2023-03-05 03:11:26 +0000

    documentation

--- workflows/bump/main.py
+++ workflows/bump/main.py
@@ -3,25 +3,38 @@ import json
 from pprint import pprint
 from semver import VersionInfo
 
+
 def bump_patch(version: str) -> str:
-    """Given a SemVer string, bump its patch part and return the result."""
+    """
+    Given a SemVer string, bumps its patch part and returns the result.
+    """
+
     parsed = VersionInfo.parse(version)
     bumped = parsed.bump_patch()
     return str(bumped)
 
+
 def update(path: str) -> None:
-    """Load a Dev Container Feature metadata file, bump the patch part of its version, and save it."""
+    """
+    Loads a Dev Container Feature metadata file, bumps the patch part of its version, and saves it.
+    """
+
     print(path)
+
     with open(path) as inFile:
         data = json.load(inFile)
+
     data['version'] = bump_patch(data['version'])
     pprint(data)
+
     with open(path, 'w') as outFile:
         json.dump(data, outFile, indent=2)
+
 
 def main() -> None:
     for metadata in glob('src/*/devcontainer-feature.json'):
         update(metadata)
+
 
 if __name__ == "__main__":
     main()
--- workflows/commit/main.py
+++ workflows/commit/main.py
@@ -3,32 +3,54 @@ import github
 import git
 from pprint import pprint
 
+
 class GitHubContentFile:
+    """
+    A file in a GitHub repo branch.
+    """
+
     def __init__(self, session: 'GitHubSession', file: github.ContentFile) -> None:
         self.session = session
         self.file = file
 
     def update(self, message: str, content: str):
+        """Commits an update of the file content.
+
+        :param message: string, Required. The commit message.
+        :param content: string, Required. The updated file content.
+        """
         repo = self.session.repo
         path = self.file.path
         sha = self.file.sha
-        ref = self.session.ref
-        return repo.update_file(path, message, content, sha, ref)
+        branch = self.session.branch
+        return repo.update_file(path, message, content, sha, branch)
+
 
 class GitHubSession:
+    """
+    A session of interactions with a GitHub repo branch.
+    """
+
     def __init__(self) -> None:
+        """
+        Opens a session.
+        """
         token = os.environ['GITHUB_TOKEN']
         repo_name = os.environ['GITHUB_REPOSITORY']
-        self.ref = os.environ['PR_BRANCH']
+        self.branch = os.environ['PR_BRANCH']
         self.repo = github.Github(token).get_repo(repo_name)
 
     def get_file(self, path: str) -> GitHubContentFile:
+        """
+        Gets the file at a given path.
+        """
         dirname = os.path.dirname(path)
-        content_list = self.repo.get_contents(dirname, self.ref)
+        content_list = self.repo.get_contents(dirname, self.branch)
         for content in content_list:
             if (content.path == path):
                 return GitHubContentFile(self, content)
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+
 
 def main():
     remote_repo = GitHubSession()
@@ -42,6 +64,7 @@ def main():
             content = local_file.read()
         result = remote_file.update(f'Auto-update {path}', content)
         pprint(result)
+
 
 if __name__ == "__main__":
     main()
