2dbc4c2cbe0582137b8ffcbcae4479b8ce66028f
hwaien <1613381+hwaien@users.noreply.github.com>
2023-03-04 19:15:41 -0800

    Refactoring code (#15)

--- workflows/bump/main.py
+++ workflows/bump/main.py
@@ -1,23 +1,40 @@
 from glob import glob
 import json
+from pprint import pprint
 from semver import VersionInfo
 
-def bump(path: str):
-    with open(path) as inFile:
-        fileContent = json.load(inFile)
-    originalVersionString = fileContent['version']
-    originalVersion = VersionInfo.parse(originalVersionString)
-    bumpedVersion = originalVersion.bump_patch()
-    bumpedVersionString = str(bumpedVersion)
-    fileContent['version'] = bumpedVersionString
-    print(path)
-    print(fileContent)
-    with open(path, 'w') as outFile:
-        json.dump(fileContent, outFile, indent=2)
 
-def main():
+def bump_patch(version: str) -> str:
+    """
+    Given a SemVer string, bumps its patch part and returns the result.
+    """
+
+    parsed = VersionInfo.parse(version)
+    bumped = parsed.bump_patch()
+    return str(bumped)
+
+
+def update(path: str) -> None:
+    """
+    Loads a Dev Container Feature metadata file, bumps the patch part of its version, and saves it.
+    """
+
+    print(path)
+
+    with open(path) as inFile:
+        data = json.load(inFile)
+
+    data['version'] = bump_patch(data['version'])
+    pprint(data)
+
+    with open(path, 'w') as outFile:
+        json.dump(data, outFile, indent=2)
+
+
+def main() -> None:
     for metadata in glob('src/*/devcontainer-feature.json'):
-        bump(metadata)
+        update(metadata)
+
 
 if __name__ == "__main__":
     main()
--- workflows/commit/main.py
+++ workflows/commit/main.py
@@ -1,25 +1,70 @@
 import os
 import github
 import git
+from pprint import pprint
+
+
+class GitHubContentFile:
+    """
+    A file in a GitHub repo branch.
+    """
+
+    def __init__(self, session: 'GitHubSession', file: github.ContentFile) -> None:
+        self.session = session
+        self.file = file
+
+    def update(self, message: str, content: str):
+        """Commits an update of the file content.
+
+        :param message: string, Required. The commit message.
+        :param content: string, Required. The updated file content.
+        """
+        repo = self.session.repo
+        path = self.file.path
+        sha = self.file.sha
+        branch = self.session.branch
+        return repo.update_file(path, message, content, sha, branch)
+
+
+class GitHubSession:
+    """
+    A session of interactions with a GitHub repo branch.
+    """
+
+    def __init__(self) -> None:
+        """
+        Opens a session.
+        """
+        token = os.environ['GITHUB_TOKEN']
+        repo_name = os.environ['GITHUB_REPOSITORY']
+        self.branch = os.environ['PR_BRANCH']
+        self.repo = github.Github(token).get_repo(repo_name)
+
+    def get_file(self, path: str) -> GitHubContentFile:
+        """
+        Gets the file at a given path.
+        """
+        dirname = os.path.dirname(path)
+        content_list = self.repo.get_contents(dirname, self.branch)
+        for content in content_list:
+            if (content.path == path):
+                return GitHubContentFile(self, content)
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+
 
 def main():
-    gh = github.Github(os.environ['GITHUB_TOKEN'])
-    remote_repo = gh.get_repo(os.environ['GITHUB_REPOSITORY'])
-    ref = os.environ['PR_BRANCH']
-
+    remote_repo = GitHubSession()
     local_repo = git.Repo('')
-    diffs = local_repo.index.diff(None)
+    diff_sequence = local_repo.index.diff(None)
+    path_sequence = (diff.a_path for diff in diff_sequence)
 
-    for d in diffs:
-        path = d.a_path
-        dirname = os.path.dirname(path)
-        remote_contents = remote_repo.get_contents(dirname, ref)
-        for c in remote_contents:
-            if (c.path == path):
-                with open(path) as inFile:
-                    content = inFile.read() 
-                remote_repo.update_file(path, f'Auto-update {path}', content, c.sha, ref)
-                break
+    for path in path_sequence:
+        remote_file = remote_repo.get_file(path)
+        with open(path) as local_file:
+            content = local_file.read()
+        result = remote_file.update(f'Auto-update {path}', content)
+        pprint(result)
+
 
 if __name__ == "__main__":
     main()
