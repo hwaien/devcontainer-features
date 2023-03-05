c81db7ebd1cbe11979e96cb648bc09d0cdb3e0ec
Hwai-En Ho <dev@hwaien.com>
2023-03-05 02:51:42 +0000

    more refactoring

--- workflows/commit/main.py
+++ workflows/commit/main.py
@@ -1,18 +1,20 @@
 import os
 import github
 import git
+from pprint import pprint
+from typing import Dict, Union
 
 class GitHubContentFile:
     def __init__(self, session: 'GitHubSession', file: github.ContentFile) -> None:
         self.session = session
         self.file = file
 
-    def update(self, message: str, content: str) -> None:
+    def update(self, message: str, content: str) -> Dict[str, Union[github.ContentFile, github.Commit]]:
         repo = self.session.repo
         path = self.file.path
         sha = self.file.sha
         ref = self.session.ref
-        repo.update_file(path, message, content, sha, ref)
+        return repo.update_file(path, message, content, sha, ref)
 
 class GitHubSession:
     def __init__(self) -> None:
@@ -32,14 +34,15 @@ class GitHubSession:
 def main():
     remote_repo = GitHubSession()
     local_repo = git.Repo('')
-    diffs = local_repo.index.diff(None)
+    diff_sequence = local_repo.index.diff(None)
+    path_sequence = (diff.a_path for diff in diff_sequence)
 
-    for changed_file in diffs:
-        path = changed_file.a_path
+    for path in path_sequence
         remote_file = remote_repo.get_file(path)
-        with open(path) as inFile:
-            content = inFile.read()
-        remote_file.update(f'Auto-update {path}', content)
+        with open(path) as local_file:
+            content = local_file.read()
+        result = remote_file.update(f'Auto-update {path}', content)
+        pprint(result)
 
 if __name__ == "__main__":
     main()
